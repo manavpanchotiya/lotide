6589816181186f65c5fa5a4e573a24630f01f3c8
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-02 16:43:24 -0500

    added solution code

---
+++ middle.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length)
+    return false;
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Arrays are equal: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Arrays are not equal: [${array1}] !== [${array2}]`);
+  }
+
+};
+
+//test
+const middle = function(array) {
+  let middleIndex = Math.floor(array.length / 2);
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[middleIndex - 1], array[middleIndex]];
+  } else {
+    return [array[middleIndex]];
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+  
