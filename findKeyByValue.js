69923bd2a7660343428b6887a12de8d33fbf48b1
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-03 17:01:03 -0500

    added soulution

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:`,   actual, ` === `,  expected);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:`, actual, ` !===`, expected);
+  }
+
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
