d3c156d07d596c521cedf57aff05b3eaa43e4c59
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 17:08:49 -0500

    refactored with module.export

--- tail.js
+++ tail.js
@@ -1,28 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:`,   actual, ` === `,  expected);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:`, actual, ` !===`, expected);
-  }
-
-};
-
 const tail = function(array) {
   return array.slice(1);
     
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-//test 1
-const words = ["hello", "world"];
-tail(words);
-assertEqual(words.length, 2);
-
-//test 2
-const empty = [];
-tail(empty);
-assertEqual(empty.length, 0);
+module.exports = tail;
