2efe210b090baba4bdafd0f845eba370527ad65b
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 17:57:10 -0500

    removed console.log and added used function to test

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,10 +2,11 @@ const middle = require("../middle");
 const assertArraysEqual = require("../assertArraysEqual");
 
 //test
-assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
+assertArraysEqual(middle([1, 2, 3]), [2]); // [2]
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
