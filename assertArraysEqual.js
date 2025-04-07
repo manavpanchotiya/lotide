fd52633110649e60b1bca11a393194981b9a5924
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 17:35:44 -0500

    refactored with module.export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length)
-    return false;
-  else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
@@ -20,6 +9,4 @@ const assertArraysEqual = function(array1, array2) {
 
 };
 
-//test
-assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); //false
+module.exports = assertArraysEqual;
