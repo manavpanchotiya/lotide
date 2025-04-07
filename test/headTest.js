f21435c6fe006b79b39684f36266a7dafadd4eef
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 18:30:20 -0500

    added missing semicolon

--- test/headTest.js
+++ test/headTest.js
@@ -13,9 +13,9 @@ describe("#head", () => {
 
   it("returns '5' for ['5, 6, 7']", () => {
     assert.strictEqual(head([5,6,7]), 5); 
-  })
+  });
   
   it("returns 'Hello' for [Hello, Lighthouse, Labs]", () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
-  })
+  });
 });
