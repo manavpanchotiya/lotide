9e747652ea76399a79aebe3bfa74b6f732b1121e
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 17:17:06 -0500

    refactored with module.export

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:`,   actual, ` === `,  expected);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:`, actual, ` !===`, expected);
-  }
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed:`,   actual, ` === `,  expected);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed:`, actual, ` !===`, expected);
+//   }
 
-};
+// };
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length)
@@ -19,5 +19,4 @@ const eqArrays = function(array1, array2) {
     return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); //false
+module.exports = eqArrays;
