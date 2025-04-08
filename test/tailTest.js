6574c815be62eff36484b62b9c5431f4af82bbea
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 19:04:33 -0500

    eslint correction

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,20 @@
 const assert = require('chai').assert;
 const tail = require("../tail");
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-const result_first = tail(["Hello", "Lighthouse", "Labs"]);
+const resultFirst = tail(["Hello", "Lighthouse", "Labs"]);
 
 describe("#tail", () => {
   it("returns tail of ['Hello', 'Lighthouse', 'Labs'] as ['Lighthouse', 'Labs]", () => {
     
-    assert.deepEqual(result, ["Lighthouse", "Labs"]); 
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
   });
 
   it("returns first element of ['Lighthouse', 'Labs] as 'Lighthouse' ", () => {
-    assert.strictEqual(result_first[0], 'Lighthouse'); 
+    assert.strictEqual(resultFirst[0], 'Lighthouse');
   });
 
   it("returns second element of ['Lighthouse', 'Labs] as 'Labs' ", () => {
-    assert.strictEqual(result_first[1], 'Labs'); 
+    assert.strictEqual(resultFirst[1], 'Labs');
   });
 
 });
