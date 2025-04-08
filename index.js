61328cc8ed9d02d149fbf4c6f6f0ed79be56a669
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 19:59:47 -0500

    added all utility functions

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
 };
+
