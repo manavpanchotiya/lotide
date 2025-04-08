5eb20681b4836d90d967ce1be779f7e0d799e6ce
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-07 19:09:27 -0500

    eslint correction

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,4 @@
 const assert = require('chai').assert;
-//const assertEqual = require("../assertEqual");
 const head = require("../head");
 
 describe("#head", () => {
@@ -8,14 +7,14 @@ describe("#head", () => {
   });
 
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
 
   it("returns '5' for ['5, 6, 7']", () => {
-    assert.strictEqual(head([5,6,7]), 5); 
+    assert.strictEqual(head([5,6,7]), 5);
   });
   
   it("returns 'Hello' for [Hello, Lighthouse, Labs]", () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
   });
 });
